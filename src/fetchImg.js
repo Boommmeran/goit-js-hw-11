1b774db12d0b907d3f64376113ae878a1ded4579
Mykhailo Volontyr <boommmeran@gmail.com>
2023-01-24 21:56:46 +0200

    add final version

---
+++ src/fetchImg.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export default class ImgFetching {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  };
+
+  async fetchImg() {
+    const BASEURL = 'https://pixabay.com/api/';
+    const KEY = '33080528-fff048daf9c61a3271b838112';
+    const PARAMETERS = `?key=${KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+    return await axios.get(`${BASEURL}${PARAMETERS}`);
+  };
+
+  plusPage() {
+    this.page += 1;
+  };
+
+  resetPage() {
+    this.page = 1;
+  };
+;
+  get query() {
+    return this.searchQuery;
+  };
+
+  set query(newQuery) {
+    return this.searchQuery = newQuery;
+  };
+}
---
+++ src/index.js
@@ -0,0 +1,100 @@
+import Notiflix from 'notiflix';
+import ImgFetching from './fetchImg';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const formRef = document.querySelector('.form');
+const submitButtonRef = document.querySelector('.form__button');
+const loadMoreBtnRef = document.querySelector('.load-more');
+const galleryRef = document.querySelector('.gallery');
+
+const fetchImgServise = new ImgFetching();
+
+formRef.addEventListener('submit', onFormSubmit);
+loadMoreBtnRef.addEventListener('click', onLoadMore);
+
+async function onFormSubmit(e) {
+  e.preventDefault();
+  fetchImgServise.page = 1;
+  loadMoreBtnRef.style.display = 'none';
+  galleryRef.innerHTML = '';
+
+  fetchImgServise.searchQuery = e.currentTarget.searchQuery.value;
+  if (fetchImgServise.searchQuery === "") {
+    Notiflix.Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  fetchImgServise.resetPage();
+  createContainer();
+  console.log(fetchImgServise.page);
+}
+
+async function onLoadMore() {
+  fetchImgServise.plusPage();
+  createContainer();
+  console.log(fetchImgServise.page);
+}
+
+async function createContainer() {
+  const response = await fetchImgServise.fetchImg();
+  if (fetchImgServise.page === 1 && response.data.totalHits !== 0) {
+    Notiflix.Notify.success(
+    `Hooray! We found ${response.data.totalHits} images.`
+  );
+  }
+  if (response.data.totalHits === 0) {
+    Notiflix.Notify.info(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    loadMoreBtnRef.style.display = 'none';
+    return;
+  }
+  loadMoreBtnRef.style.display = 'block';
+
+  marcup(response.data.hits);
+}
+
+function marcup(response) {
+  const galleryItem = response
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="container">
+        <a class="gallery__link" href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info__item">
+            <b>Likes ${likes}</b>
+          </p>
+          <p class="info__item">
+            <b>Views ${views}</b>
+          </p>
+          <p class="info__item">
+            <b>Comments ${comments}</b>
+          </p>
+          <p class="info__item">
+            <b>Downloads ${downloads}</b>
+          </p>
+        </div>
+        </a>
+      </div>`;
+      }
+    )
+    .join('');
+  galleryRef.insertAdjacentHTML('beforeEnd', galleryItem);
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    scrollZoom: false,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+};
